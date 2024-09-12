bb3b1d91efca52dc43ecdcc110820d42131b0446
na-ah <na.ah.0006@gmail.com>
2024-09-12 20:40:05 +0900

    chore: verceのWebAnalyticsを追加

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,13 @@
 import "@/styles/globals.css";
 import { MantineProvider } from "@mantine/core";
 import "@mantine/core/styles.css";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App({ Component, pageProps }) {
   return (
     <MantineProvider>
       <Component {...pageProps} />
+      <Analytics />
     </MantineProvider>
   );
 }
