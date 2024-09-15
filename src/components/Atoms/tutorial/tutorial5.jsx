c4c03a2b0e27962f6c3dbe2c23a0468d6437ca77
na-ah <na.ah.0006@gmail.com>
2024-09-15 14:38:33 +0900

    feat: Tutorial5のサンプルIDを変更

--- src/components/Atoms/tutorial/tutorial5.jsx
+++ src/components/Atoms/tutorial/tutorial5.jsx
@@ -15,7 +15,7 @@ export default function Tutorial5() {
         title={"再生リストをまとめて登録しよう"}
         progress={"5/6"}
         content={
-          "お疲れさまでした！\n基本的なTimerVidの使い方を学ぶことができました！\n\n最後に、Youtubeの再生リストを一気に登録する方法を学びましょう。再生リストのURLまたは、再生リストID（例：PLU1XqNAUBP5Vp2fOpaUV3aYa6XwfkL73f）をコピーしておいてください。もう一度skipを押して、workタイマーに移動してから次のボタンを押してください。"
+          "お疲れさまでした！\n基本的なTimerVidの使い方を学ぶことができました！\n\n最後に、Youtubeの再生リストを一気に登録する方法を学びましょう。再生リストのURLまたは、再生リストID（例：PLn4Dv716EsE2VkKVbP0D9RowcelVrFoQh）をコピーしておいてください。もう一度skipを押して、workタイマーに移動してから次のボタンを押してください。"
         }
         buttonLabel={"⑤ 再生リストを一気に登録する方法を知る"}
       />
