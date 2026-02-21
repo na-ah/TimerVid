c9866627eb12b482c6d900144610d72db557bb74
na-ah <naruhito.takata@vpage.co.jp>
2026-02-21 13:14:18 +0900

    feat: 2カラムレイアウトとシアターモードを追加

--- src/components/Organisms/playlist/playlistContainer.jsx
+++ src/components/Organisms/playlist/playlistContainer.jsx
@@ -1,4 +1,4 @@
-import { useContext, useMemo, useState } from "react";
+import { useContext, useState } from "react";
 import PlaylistTabs from "../../Molecules/playlist/playlistTabs";
 import { PlaylistContext } from "../../../context/playlistProvider";
 import PlaylistInfo from "../../Molecules/playlist/playlistInfo";
@@ -8,7 +8,7 @@ import { GoArrowSwitch } from "react-icons/go";
 import { TbPlayerTrackNextFilled } from "react-icons/tb";
 import { TbPlayerTrackPrevFilled } from "react-icons/tb";
 import PlaylistAddVideoListModal from "../../Molecules/playlist/playlistAddVideoListModal";
-import { FaPlus } from "react-icons/fa6";
+import { FaPlus, FaExpand, FaCompress } from "react-icons/fa6";
 import PlaylistAddModal from "../../Molecules/playlist/playlistAddModal";
 
 export default function PlaylistContainer({ children }) {
@@ -16,7 +16,6 @@ export default function PlaylistContainer({ children }) {
   const { breakPlaylist } = useContext(PlaylistContext);
   const {
     isWorking,
-    setIsWorking,
     switchStatus,
     selectedPlaylist,
     setSelectedPlaylist,
@@ -25,79 +24,112 @@ export default function PlaylistContainer({ children }) {
     removePlaylist,
   } = useContext(PlaylistContext);
 
+  const [isCinemaMode, setIsCinemaMode] = useState(false);
+
   return (
     <>
-      <div className="flex flex-col items-center w-full">
-        <div className="my-5">
-          {isWorking ? (
-            <PlaylistInfo {...workPlaylist} />
-          ) : (
-            <PlaylistInfo {...breakPlaylist} />
-          )}
-        </div>
-        {children}
-        <Tabs
-          className="w-full my-5"
-          defaultValue={isWorking ? "work" : "break"}
-          value={selectedPlaylist}
-          onChange={setSelectedPlaylist}
+      <div className="flex flex-col md:flex-row gap-6 w-full items-start">
+        {/* Left Column: Player & Current Info */}
+        <div
+          className={`flex flex-col w-full transition-all duration-500 ease-in-out ${
+            isCinemaMode ? "md:w-9/12" : "md:w-5/12"
+          }`}
         >
-          <Tabs.List className="flex items-center w-full">
-            <Tabs.Tab value="work">work playlist</Tabs.Tab>
-            <Tabs.Tab value="break">
-              <span id="tutorial4-2">break playlist</span>
-            </Tabs.Tab>
-            <div className="ml-auto flex items-center gap-4 px-2 pb-2">
-              <Group spacing="xs" className="hidden sm:flex">
-                <Tooltip label="作業/休憩を切り替え" position="bottom" withArrow>
-                  <ActionIcon variant="light" color="blue" onClick={switchStatus} size="lg">
-                    <GoArrowSwitch size={20} />
-                  </ActionIcon>
-                </Tooltip>
-                <Tooltip label="前の動画" position="bottom" withArrow>
-                  <ActionIcon variant="light" color="blue" onClick={prevVideo} size="lg">
-                    <TbPlayerTrackPrevFilled size={20} />
-                  </ActionIcon>
-                </Tooltip>
-                <Tooltip label="次の動画" position="bottom" withArrow>
-                  <ActionIcon variant="light" color="blue" onClick={nextVideo} size="lg">
-                    <TbPlayerTrackNextFilled size={20} />
-                  </ActionIcon>
-                </Tooltip>
-              </Group>
-              
-              <Button
-                id="tutorial2-1"
-                leftSection={<FaPlus />}
-                variant="outline"
-                size="sm"
-                onClick={() => {
-                  selectedPlaylist == "work"
-                    ? workPlaylist.openAddPlaylist()
-                    : breakPlaylist.openAddPlaylist();
-                }}
+          <div className="mb-2 flex justify-end">
+            <Tooltip label={isCinemaMode ? "リスト表示モード" : "シアターモード"} position="left" withArrow>
+              <ActionIcon
+                variant="light"
+                color="gray"
+                onClick={() => setIsCinemaMode(!isCinemaMode)}
+                size="lg"
               >
-                新規リスト
-              </Button>
-            </div>
-          </Tabs.List>
-          <Tabs.Panel value="work">
-            <div className="my-8">
-              <PlaylistAddVideoListModal {...workPlaylist} />
-              <PlaylistAddVideoModal {...workPlaylist} />
-              <PlaylistAddModal {...workPlaylist} />
-              <PlaylistTabs {...workPlaylist} removePlaylist={removePlaylist} isWorking={isWorking} tabMode="work" />
-            </div>
-          </Tabs.Panel>
-          <Tabs.Panel value="break">
-            <div className="my-8">
-              <PlaylistAddVideoListModal {...breakPlaylist} />
-              <PlaylistAddVideoModal {...breakPlaylist} />
-              <PlaylistAddModal {...breakPlaylist} />
-              <PlaylistTabs {...breakPlaylist} removePlaylist={removePlaylist} isWorking={isWorking} tabMode="break" />
-            </div>
-          </Tabs.Panel>
-        </Tabs>
+                {isCinemaMode ? <FaCompress size={20} /> : <FaExpand size={20} />}
+              </ActionIcon>
+            </Tooltip>
+          </div>
+
+          <div className="mb-4">
+            {isWorking ? (
+              <PlaylistInfo {...workPlaylist} />
+            ) : (
+              <PlaylistInfo {...breakPlaylist} />
+            )}
+          </div>
+          
+          <div className="w-full shadow-lg rounded-lg overflow-hidden">
+            {children}
+          </div>
+        </div>
+
+        {/* Right Column: Playlist Selection */}
+        <div
+          className={`w-full transition-all duration-500 ease-in-out ${
+            isCinemaMode ? "md:w-3/12" : "md:w-7/12"
+          }`}
+        >
+          <Tabs
+            className="w-full"
+            defaultValue={isWorking ? "work" : "break"}
+            value={selectedPlaylist}
+            onChange={setSelectedPlaylist}
+          >
+            <Tabs.List className="flex items-center w-full flex-wrap">
+              <Tabs.Tab value="work">work playlist</Tabs.Tab>
+              <Tabs.Tab value="break">
+                <span id="tutorial4-2">break playlist</span>
+              </Tabs.Tab>
+              <div className="ml-auto flex items-center gap-2 px-2 pb-2 mt-2 sm:mt-0">
+                <Group spacing="xs" className="hidden xl:flex">
+                  <Tooltip label="作業/休憩を切り替え" position="bottom" withArrow>
+                    <ActionIcon variant="light" color="blue" onClick={switchStatus} size="lg">
+                      <GoArrowSwitch size={20} />
+                    </ActionIcon>
+                  </Tooltip>
+                  <Tooltip label="前の動画" position="bottom" withArrow>
+                    <ActionIcon variant="light" color="blue" onClick={prevVideo} size="lg">
+                      <TbPlayerTrackPrevFilled size={20} />
+                    </ActionIcon>
+                  </Tooltip>
+                  <Tooltip label="次の動画" position="bottom" withArrow>
+                    <ActionIcon variant="light" color="blue" onClick={nextVideo} size="lg">
+                      <TbPlayerTrackNextFilled size={20} />
+                    </ActionIcon>
+                  </Tooltip>
+                </Group>
+                
+                <Button
+                  id="tutorial2-1"
+                  leftSection={<FaPlus />}
+                  variant="outline"
+                  size="xs"
+                  onClick={() => {
+                    selectedPlaylist == "work"
+                      ? workPlaylist.openAddPlaylist()
+                      : breakPlaylist.openAddPlaylist();
+                  }}
+                >
+                  新規
+                </Button>
+              </div>
+            </Tabs.List>
+            <Tabs.Panel value="work">
+              <div className="my-4">
+                <PlaylistAddVideoListModal {...workPlaylist} />
+                <PlaylistAddVideoModal {...workPlaylist} />
+                <PlaylistAddModal {...workPlaylist} />
+                <PlaylistTabs {...workPlaylist} removePlaylist={removePlaylist} isWorking={isWorking} tabMode="work" />
+              </div>
+            </Tabs.Panel>
+            <Tabs.Panel value="break">
+              <div className="my-4">
+                <PlaylistAddVideoListModal {...breakPlaylist} />
+                <PlaylistAddVideoModal {...breakPlaylist} />
+                <PlaylistAddModal {...breakPlaylist} />
+                <PlaylistTabs {...breakPlaylist} removePlaylist={removePlaylist} isWorking={isWorking} tabMode="break" />
+              </div>
+            </Tabs.Panel>
+          </Tabs>
+        </div>
       </div>
     </>
   );
--- src/components/Templates/TimerVid.jsx
+++ src/components/Templates/TimerVid.jsx
@@ -14,7 +14,7 @@ export default function TimerVid() {
   return (
     <>
       <MantineProvider>
-        <div className="min-h-dvh min-w-[240px] w-full sm:w-[640px] md:w-[768px] xl:w-[1280px] mx-auto">
+        <div className="min-h-dvh min-w-[240px] w-full max-w-[1920px] mx-auto px-4">
           <Toaster />
           <Header setIsShowTutorial={setIsShowTutorial} />
           <TutorialContainer
